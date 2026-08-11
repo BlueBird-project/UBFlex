aa00c9c12060ad231e7f1d293ddceada63602350
tomasz_ciesielczyk <tomaszc@man.poznan.pl>
2026-08-11 14:13:53 +0200

    init py ubflex library

---
+++ ontology/PyUBFlex/PyUBFlex/ubflex/rdf/__init__.py
@@ -0,0 +1,6 @@
+from rdflib import URIRef
+
+UBFLEX_MARKET_BASE = "https://ubflex.bluebird.eu/market/"
+
+EURPerMWH = URIRef(value="EURPerMegawattHour", base=UBFLEX_MARKET_BASE)
+PLNPerMWH = URIRef(value="PLNPerMegawattHour", base=UBFLEX_MARKET_BASE)
