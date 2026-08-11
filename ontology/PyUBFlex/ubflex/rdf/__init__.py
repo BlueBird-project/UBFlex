42ef9186d22697a4c6935a791786bcd21dbb38e4
tomasz_ciesielczyk <tomaszc@man.poznan.pl>
2026-08-11 17:23:37 +0200

    update py ubflex

--- ontology/PyUBFlex/ubflex/rdf/__init__.py
+++ ontology/PyUBFlex/ubflex/rdf/__init__.py
@@ -4,3 +4,7 @@ UBFLEX_MARKET_BASE = "https://ubflex.bluebird.eu/market/"
 
 EURPerMWH = URIRef(value="EURPerMegawattHour", base=UBFLEX_MARKET_BASE)
 PLNPerMWH = URIRef(value="PLNPerMegawattHour", base=UBFLEX_MARKET_BASE)
+
+
+DAYAHEAD_MARKET_TYPE = URIRef(value="DayAheadMarket", base=UBFLEX_MARKET_BASE)
+INTRADAY_MARKET_TYPE = URIRef(value="IntradayMarket", base=UBFLEX_MARKET_BASE)
